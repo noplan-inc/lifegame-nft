7582e04ff78dd0b2347ed1b708efacf141f6d197
serinuntius <wasab1gj@gmail.com>
2021-02-18 15:43:47 +0900

    impl cell loader

---
+++ src/utils/loader.ts
@@ -0,0 +1,15 @@
+import { restoreCells } from './helper';
+
+export const loadCellsFromContentUrl = async (url: string): Promise<Cell[]> => {
+    const resp = await fetch(url);
+    if (!resp.ok) {
+        return [];
+    }
+
+    const exportedCell = await resp.json();
+
+    if (!exportedCell.size) {
+        return [];
+    }
+    return restoreCells(exportedCell);
+};
