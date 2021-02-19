581e3275eacb69e9669fd6ad6226dc996131bf05
serinuntius <wasab1gj@gmail.com>
2021-02-19 19:39:12 +0900

    add firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCmPAKZls4RxXBp0M_qBm8ztoPSNEDB_DU',
+    authDomain: 'lifegame-nft.firebaseapp.com',
+    projectId: 'lifegame-nft',
+    storageBucket: 'lifegame-nft.appspot.com',
+    messagingSenderId: '1039742837872',
+    appId: '1:1039742837872:web:252d53a52fffd5bf39c0b5',
+};
