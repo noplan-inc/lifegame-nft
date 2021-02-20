e1a1a6fe46a152bb08aabef8b5992b41f691217b
serinuntius <wasab1gj@gmail.com>
2021-02-21 08:31:10 +0900

    impl className for outside

--- src/presentational/button.tsx
+++ src/presentational/button.tsx
@@ -1,23 +1,30 @@
 import React, { ReactNode } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 interface TWButtonProps {
     onClick: () => void;
+    className?: string;
     children: ReactNode;
 }
-export const TWButton: React.FC<TWButtonProps> = ({ children, onClick }) => {
+export const TWButton: React.FC<TWButtonProps> = ({
+    className,
+    children,
+    onClick,
+}) => {
+    const c = classNames(
+        'text-white px-3 py-2 rounded-md text-sm font-medium bg-blue-900 mx-4',
+        className
+    );
     return (
-        <button
-            type={'button'}
-            onClick={onClick}
-            className="text-white px-3 py-2 rounded-md text-sm font-medium bg-blue-900 mx-4"
-        >
+        <button type={'button'} onClick={onClick} className={c}>
             {children}
         </button>
     );
 };
 
 TWButton.propTypes = {
+    className: PropTypes.string,
     children: PropTypes.element.isRequired,
     onClick: PropTypes.func.isRequired,
 };
