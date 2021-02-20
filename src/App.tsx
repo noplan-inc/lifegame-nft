4f76240898713397a90aa48180831ee8a84b0a54
serinuntius <wasab1gj@gmail.com>
2021-02-20 17:20:13 +0900

    impl router

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import { ethFetcher } from 'swr-eth';
 import { SWRConfig } from 'swr';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import {
     NotificationContainer,
@@ -14,14 +15,17 @@ import {
 } from 'react-notifications';
 
 import { firebaseConfig } from './config/firebase';
-import Board from './container/board';
+
 import './App.css';
-import BoardView from './presentational/boardView';
+
 import { useEagerConnect, useInactiveListener } from './hooks';
 import { injected } from './utils/connectors';
-import { ListZora } from './container/zora';
+
 import BEP20ABI from './abi/BEP20.abi.json';
 import { Bep20 } from './addresses/bsc-testnet-bep20';
+import { Home } from './pages/home';
+import { Editor } from './pages/editor';
+import { Collection } from './pages/collection';
 
 const ABIs = Object.values(Bep20).map((address) => {
     const abi = BEP20ABI as any;
@@ -34,9 +38,7 @@ function App() {
     const {
         connector,
         active,
-        error,
         activate,
-        setError,
         library,
     } = useWeb3React<Web3Provider>();
 
@@ -62,54 +64,61 @@ function App() {
 
     return (
         <div className="App">
-            {!active || !library ? (
-                <div>
-                    <button
-                        onClick={async () => {
-                            try {
-                                await activate(injected);
-                            } catch (e) {
-                                NotificationManager.error(JSON.stringify(e));
-                                console.error(e);
-                            }
+            <Router>
+                {!active || !library ? (
+                    <div>
+                        <button
+                            onClick={async () => {
+                                try {
+                                    await activate(injected);
+                                } catch (e) {
+                                    NotificationManager.error(
+                                        JSON.stringify(e)
+                                    );
+                                    console.error(e);
+                                }
+                            }}
+                        >
+                            wallet connect
+                        </button>
+                    </div>
+                ) : (
+                    <SWRConfig
+                        value={{
+                            fetcher: ethFetcher(
+                                library, // @ts-ignore
+                                new Map<string, any>(ABIs)
+                            ),
                         }}
                     >
-                        wallet connect
-                    </button>
-                </div>
-            ) : (
-                <SWRConfig
-                    value={{
-                        fetcher: ethFetcher(
-                            library, // @ts-ignore
-                            new Map<string, any>(ABIs)
-                        ),
-                    }}
-                >
-                    <NotificationContainer />
-                    <div>
-                        {JSON.stringify({ active, error, activate, setError })}
-                    </div>
-
-                    <div>
-                        <h2>editor</h2>
-                        <Board
-                            mode={'editor'}
-                            intervalTime={500}
-                            cellSize={20}
-                            spawnRate={25}
-                            boardSize={25}
-                            render={(props: BoardViewProps) => (
-                                <BoardView {...props} />
-                            )}
-                        />
-                    </div>
-                    <div>
-                        <h2>market</h2>
-                        <ListZora />
-                    </div>
-                </SWRConfig>
-            )}
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to={'/'}>home</Link>
+                                </li>
+                                <li>
+                                    <Link to={'/editor'}>editor</Link>
+                                </li>
+                                <li>
+                                    <Link to={'/collection'}>Collection</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <NotificationContainer />
+                        <Switch>
+                            <Route exact path={'/'}>
+                                <Home />
+                            </Route>
+                            <Route path={'/editor'}>
+                                <Editor />
+                            </Route>
+                            <Route path={'/collection'}>
+                                <Collection />
+                            </Route>
+                        </Switch>
+                    </SWRConfig>
+                )}
+            </Router>
         </div>
     );
 }
